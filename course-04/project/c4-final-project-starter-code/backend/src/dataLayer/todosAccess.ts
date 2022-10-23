112df21957abde430e287b4d8906fa7894dfd5c3
Prince Narteh <prinarteh@gmail.com>
2022-10-23 04:44:18 +0000

    adds todoUpdate data access

--- course-04/project/c4-final-project-starter-code/backend/src/dataLayer/todosAccess.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/dataLayer/todosAccess.ts
@@ -1,20 +1,18 @@
 import * as AWS from 'aws-sdk'
-import {Types} from "aws-sdk/clients/s3" 
+import { Types } from 'aws-sdk/clients/s3'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { TodoItem } from '../models/TodoItem'
 import { TodoUpdate } from '../models/TodoUpdate'
-
 
 export class TodosAccess {
   constructor(
     private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
     private readonly s3Bucket = process.env.S3_BUCKET_NAME,
-    private readonly s3Client: Types = new AWS.S3({signatureVersion: 'v4'}),
-    private readonly todosTable = process.env.GROUPS_TABLE,
+    private readonly s3Client: Types = new AWS.S3({ signatureVersion: 'v4' }),
+    private readonly todosTable = process.env.GROUPS_TABLE
   ) {}
 
   createTodo = async (todoItem: TodoItem): Promise<TodoItem> => {
-    
     await this.docClient
       .put({
         TableName: this.todosTable,
@@ -33,7 +31,7 @@ export class TodosAccess {
         TableName: this.todosTable,
         KeyConditionExpression: '#userId = :userId',
         ExpressionAttributeNames: {
-          '#userId': "userId"
+          '#userId': 'userId'
         },
         ExpressionAttributeValues: {
           ':userId': userId
@@ -45,10 +43,35 @@ export class TodosAccess {
     return items as TodoItem[]
   }
 
-  async getTodoById(todoId: string): Promise<TodoItem> {
-    const result = await this.docClient.query({
-      TableName: this.todosTable
-    })
-    return result.
+  async updateTodo(
+    todoUpdate: TodoUpdate,
+    todoId: string,
+    userId: string
+  ): Promise<TodoUpdate> {
+    const res = await this.docClient
+      .update({
+        TableName: this.todosTable,
+        Key: {
+          userId: userId,
+          todoId: todoId
+        },
+        UpdateExpression:
+          'set #name = :name, #dueDate = :dueDate, #done = :done',
+        ExpressionAttributeNames: {
+          '#name': 'name',
+          '#dueDate': 'dueDate',
+          '#done': 'done'
+        },
+        ExpressionAttributeValues: {
+          ':name': todoUpdate['name'],
+          ':dueDate': todoUpdate['dueDate'],
+          ':done': todoUpdate['done']
+        },
+        ReturnValues: 'ALL_NEW'
+      })
+      .promise()
+
+    const attributes = res.Attributes
+    return attributes as TodoUpdate
   }
 }
