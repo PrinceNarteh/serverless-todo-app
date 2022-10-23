c665dd1fb6c3e29fcc09992055837235ed8ab4c4
Prince Narteh <prinarteh@gmail.com>
2022-10-23 05:29:07 +0000

    refactor auth0Authorizer function

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,5 +1,6 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
+import Axios from 'axios'
 
 import { verify } from 'jsonwebtoken'
 import { JwtPayload } from '../../auth/JwtPayload'
@@ -7,8 +8,7 @@ import { createLogger } from '../../utils/logger'
 
 const logger = createLogger('auth')
 
-const secretId = process.env.AUTH_0_SECRET_ID
-// const jwksUrl = 'https://dev-5sxcycy5.us.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-5sxcycy5.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -51,13 +51,18 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  //   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  try {
+    const token = getToken(authHeader)
+    const res = await Axios.get(jwksUrl)
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return verify(token, secretId) as JwtPayload
+    // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
+    const pemData = res['data']['keys'][0]['x5c'][0]
+    const cert = `-----BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----`
+
+    return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+  } catch (err) {
+    logger.error('Fail to authenticate', err)
+  }
 }
 
 function getToken(authHeader: string): string {
